2c27d983b68c34bd2a0386bf30af23ccf2b4e051
Jmacea <jeisonalejandro2002@gmail.com>
2024-08-21 20:05:56 -0400

    [ADD] First Commit

---
+++ tailwind.config.cjs
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+
+export default {
+    content: ['./src/**/*/*.{astro,html,js,jsx}'],
+    theme: {
+        extend: {
+            animation: {
+                border: 'background ease infinite',
+                    },
+                keyframes: {
+                background: {
+                  '0%, 100%': { backgroundPosition: '0% 50%' },
+                  '50%': { backgroundPosition: '100% 50%' },
+                },
+            },
+        }
+    },
+    plugins: []
+}
